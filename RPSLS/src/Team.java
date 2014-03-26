bf2e3ee0b7f527ec5d94852637d0c05f756ecdf1
Alex Tarng <alecadad@gmail.com>
2014-03-26 13:33:57 -0700

    added instance variable, constructor, and add and delete

--- RPSLS/src/Team.java
+++ RPSLS/src/Team.java
@@ -1,5 +1,20 @@
 
 public class Team 
 {
-
+	private Element[] team = new Element[10];
+	
+	public Team()
+	{
+		
+	}
+	
+	public void add(Element e, int index)
+	{
+		team[index] = e;
+	}
+	
+	public void delete(int index)
+	{
+		team[index] = null;
+	}
 }
