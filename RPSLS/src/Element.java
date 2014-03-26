45865eef84d803d801c44b8bbd332c9946b362fa
bencw99 <ben.cohenwang@gmail.com>
2014-03-26 13:34:39 -0700

    made element stuff

--- RPSLS/src/Element.java
+++ RPSLS/src/Element.java
@@ -1,6 +1,20 @@
 
 public class Element 
 {
-	
+	private static String[] elements;
+	private String name;
+	private int iD;
+	public boolean compare(Element other)
+	{
+		int gap = Math.abs(this.iD - other.iD);
+		if(gap % 2 == 0)
+		{
+			return Math.min(this.iD, other.iD) == this.iD;
+		}
+		else
+		{
+			return Math.max(this.iD, other.iD) == other.iD;
+		}
+	}
 }
 
