90535db1ff493b022e90a6eb7a234796656bb7bb
bencw99 <ben.cohenwang@gmail.com>
2014-03-26 21:03:43 -0700

    Update Team.java

--- RPSLS/src/Team.java
+++ RPSLS/src/Team.java
@@ -1,4 +1,3 @@
-
 public class Team 
 {
 	private Element[] team = new Element[10];
@@ -8,13 +7,34 @@ public class Team
 		
 	}
 	
-	public void add(Element e, int index)
+	public boolean battle(Team other)
 	{
-		team[index] = e;
+		Element[] otherTeam = other.getTeam();
+		for(int i = 0; i < Max(otherTeam.length, team.length)); i++)
+		{
+			
+		}
+	}
+	
+	public void add(Element e)
+	{
+		try
+		{
+			team[team.indexOf(null)] = e;
+		}
+		catch(Exception e)
+		{
+			//Team filled
+		}
 	}
 	
 	public void delete(int index)
 	{
 		team[index] = null;
+	}
+	
+	public Element[] getTeam()
+	{
+		return team;
 	}
 }
