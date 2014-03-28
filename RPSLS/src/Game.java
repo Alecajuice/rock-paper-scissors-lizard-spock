4d2d6a69ccc574553ec7cda6b2aff2e338706454
bencw99 <ben.cohenwang@gmail.com>
2014-03-28 13:06:21 -0700

    New classes

---
+++ RPSLS/src/Game.java
@@ -0,0 +1,15 @@
+import java.awt.Graphics;
+public class Game 
+{
+	Player player1;
+	Player player2;
+	public Game(Player player1, Player player2)
+	{
+		this.player1 = player1;
+		this.player2 = player2;
+	}
+	public void draw(Graphics screen)
+	{
+		
+	}
+}
