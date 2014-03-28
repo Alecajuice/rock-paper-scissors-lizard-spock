e26404141a95e4a938296db0cb5f372c61f95b93
bencw99 <ben.cohenwang@gmail.com>
2014-03-28 12:51:53 -0700

    updated Player

--- RPSLS/src/Player.java
+++ RPSLS/src/Player.java
@@ -1,5 +1,14 @@
 
 public class Player 
 {
-	Team team = new Team();
+	private Team team = new Team();
+	private String name;
+	public Player(String name)
+	{
+		this.name = name;
+	}
+	public Element getElement(int index)
+	{
+		return team.getElement(index);
+	}
 }
